b1c8a4b945a392eb4e330d338ddde310e08cc796
Yasushi Sakai <ysshski@gmail.com>
2016-08-17 15:29:54 -0400

    Squashed commit of the following:
    commit abe85c2966fc92396aad66928a0efb65dd57af0f
    Merge: c9d806f 01d28d6
    Author: Yasushi Sakai <ysshski@gmail.com>
    Date:   Wed Aug 17 15:28:15 2016 -0400
    
        ??
    
    commit c9d806f6b5e07283ada4fb7191bae6b421da50e0
    Author: Yasushi Sakai <ysshski@gmail.com>
    Date:   Wed Aug 17 15:26:50 2016 -0400
    
        reciever code
    
    commit 1f9c5a982cbd05ecfb9949764df25dcc1baabf66
    Author: Yasushi Sakai <ysshski@gmail.com>
    Date:   Wed Aug 17 15:07:16 2016 -0400
    
        incomplete push
    
    commit cd735143c8296323782daecb14a5ca775d31dbf9
    Author: Yasushi Sakai <ysshski@gmail.com>
    Date:   Wed Aug 17 14:33:01 2016 -0400
    
        image receiver test
    
    commit 01d28d6e31e7ad44c4eba44d240296d9aa34051e
    Author: Yasushi Sakai <ysshski@gmail.com>
    Date:   Wed Aug 17 15:07:16 2016 -0400
    
        incomplete push
    
    commit bdf4fb92f6d1a4ca21ed3eb615644c86202c173d
    Author: Yasushi Sakai <ysshski@gmail.com>
    Date:   Wed Aug 17 14:33:01 2016 -0400
    
        image receiver test

---
+++ Utilities/ImageReceiver/src/ImageReceiverApp.cpp
@@ -0,0 +1,136 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+#include "cinder/Serial.h"
+#include "cinder/Log.h"
+#include "cinder/params/Params.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+const int BAUD_RATE = 9600;
+const char NUM_BYTES = 1;
+
+const ivec2 windowSize(1280+640,720);
+
+class ImageReceiverApp : public App {
+  public:
+	void setup() override;
+	void mouseDown( MouseEvent event ) override;
+    void keyDown(KeyEvent event) override;
+	void update() override;
+	void draw() override;
+    
+    
+  private:
+    void initPort();
+    void initFBO();
+    
+    Surface             mSendImage;
+    Surface             mPixelImage;
+    
+    // Serial
+    SerialRef           mSerial;
+    string              mSerialName;
+    string              mSerialStr;
+    uint8_t*            mSendData;
+    
+    // FBO
+    gl::FboRef          mFbo;
+    
+    
+    ivec2               mIteraPixel;
+    ColorA              mCurrentColor; // can this be char??
+    bool                mFinishSending;
+    bool                mPixelReady;
+    bool                mSendPixels;
+    bool                mDrawOriginal;
+    
+    Font                mFont;
+    gl::Texture2dRef    mTextTexture;
+    
+    params::InterfaceGlRef mParams;
+    float               mAvgFps;
+    
+
+};
+
+void ImageReceiverApp::setup()
+{
+    setWindowSize(windowSize);
+    mSendImage = loadImage(loadAsset("wave.png"));
+    
+    initFBO();
+    initPort();
+    
+    // we don't need to process the image.. just recieve it!
+    
+    mIteraPixel    = ivec2(0,0);
+    mFinishSending = false;
+    mPixelReady    = false;
+    mSendPixels    = false;
+    mDrawOriginal  = false;
+    
+    
+    mFont = Font("Arial",20);
+    mTextTexture   = Area(0,0,width,height);
+    
+    mParams = params::InterfaceGlRef::create(getWindow(),"App Parameters", toPixels(ivec(200,200)));
+    mParams->addParam("FPS",&mAvgFps,false);
+    mParams->addParam("Draw",&mDrawOriginal);
+}
+
+void ImageReceiverApp::mouseDown( MouseEvent event )
+{
+}
+
+void ImageReceiverApp::keyDown(KeyEvent event){
+    
+}
+
+void ImageReceiverApp::update()
+{
+    mAvgFps = getAverageFps();
+    
+}
+
+void ImageReceiverApp::draw()
+{
+	gl::clear( Color( 0, 0, 0 ) ); 
+}
+
+
+void ImageReceiverApp::initPort(){
+    // print the devices
+    string findUsb = "cu.usbserial";
+    for( const auto &dev : Serial::getDevices() ){
+        CI_LOG_V("Device: " << dev.getName() );
+    }try {
+        Serial::Device dev = Serial::findDeviceByNameContains( "cu.usbserial" );
+        mSerialName = dev.getName();
+        mSerial = Serial::create( dev, BAUD_RATE );
+        CI_LOG_I( "Connected to: "<< mSerialName);
+    }
+    catch( SerialExc &exc ) {
+        CI_LOG_EXCEPTION( "coult not initialize the serial device", exc );
+        exit( -1 );
+    }
+    mSendData = new uint8_t[NUM_BYTES];
+}
+
+void ImageReceiverApp::initFBO(){
+    //FBO
+    mFbo = gl::Fbo::create(mSendImage.getWidth(), mSendImage.getHeight());
+    {
+        gl::ScopedFramebuffer fbScp( mFbo );
+        
+        gl::clear( Color( 0.0, 0.0, 0.0 ) );
+        
+        gl::ScopedViewport scpVp( ivec2( 0 ), mFbo->getSize() );
+        
+    }
+}
+
+
+CINDER_APP( ImageReceiverApp, RendererGl )
