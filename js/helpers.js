6833b49469422f54ef909491695e8371877679a5
Yasushi Sakai <ysshski@gmail.com>
2016-12-27 16:56:17 -0500

    video tag

---
+++ js/helpers.js
@@ -0,0 +1,13 @@
+
+window.onload=function(){
+	
+	// small enough volume
+	var video = document.getElementById("video");
+
+	video.volume = 0.05;	
+	console.log("volume to 20%");
+
+	window.scrollTo(0,1);
+
+
+};
