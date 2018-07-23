16f5b5bb2021ff8d99de3ef042b751dd73c9e5b7
Yasushi Sakai <ysshski@gmail.com>
2018-07-23 10:50:05 -0400

    cleeeeean up

--- Utilities/ImageReceiver/xcode/build/ImageReceiver.build/Debug/ImageReceiver.build/Objects-normal/x86_64/ImageReceiverApp.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/yasushisakai/code/talkingdrums/Utilities/ImageReceiver/src/ImageReceiverApp.cpp \
-  ../../../../cinder/include/cinder/app/RendererGl.h \
-  ../../../../cinder/include/cinder/Serial.h \
-  ../../../../cinder/include/cinder/Log.h \
-  ../../../../cinder/include/cinder/System.h
